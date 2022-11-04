8ac3c00c7a294dc28730a1e1bd4aa8d9842d1615
Adriana <adrianagb2105@gmail.com>
2022-11-03 22:14:44 -0600

    primer commit
    proyecto control de presupuesto

---
+++ modal.js
@@ -0,0 +1,11 @@
+// modal del carrito
+const contenedorModal = document.getElementsByClassName('modal-contenedor')[0];
+const botonAbrir = document.getElementById('boton-lista');
+const botonfiltro=document.getElementById('boton-filtro')
+const botonCerrar = document.getElementById('listaGastosCerrar')
+botonAbrir.addEventListener('click', ()=>{
+    contenedorModal.classList.toggle('modal-active')
+});
+botonCerrar.addEventListener('click', ()=>{
+    contenedorModal.classList.toggle('modal-active')
+});
